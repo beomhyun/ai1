78e18f67bd71fb9b839ce5f66c08723870c13fda
beomhyunkim <beomhyunkim@gmail.com>
2019-08-23 09:19:38 +0900

    end

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 
 
-SLACK_TOKEN = None
-SLACK_SIGNING_SECRET = None
+SLACK_TOKEN = "xoxb-656725000466-731579237621-NEhqRSQjtDvS2oZ1PJyrOb0r"
+SLACK_SIGNING_SECRET = "599d518ad04b54b6915dedf2c567c399"
 
 app = Flask(__name__)
 
@@ -15,17 +15,31 @@ slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app
 slack_web_client = WebClient(token=SLACK_TOKEN)
 
 # Req. 2-1-1. pickle로 저장된 model.clf 파일 불러오기
-
+loaded_model = pickle.load(open("model.clf", 'rb'))
+beta_0 = loaded_model.coef_[0][0]
+beta_1 = loaded_model.coef_[0][1]
+beta_2 = loaded_model.coef_[0][2]
+beta_3 = loaded_model.intercept_[0]
 # Req. 2-1-2. 입력 받은 광고비 데이터에 따른 예상 판매량을 출력하는 lin_pred() 함수 구현
 def lin_pred(test_str):
+    test_str = test_str.replace("<@UMHH16ZJ9> ","")
 
-    return None
+    arr = test_str.split(" ")
+    tv = arr[0]
+    rd = arr[1]
+    newspaper = arr[2]
+
+    rs = ((float(tv) * beta_0 ) + (float(rd) * beta_1) + (float(newspaper) * beta_2 + beta_3))
+    return rs
 
 # 챗봇이 멘션을 받았을 경우
 @slack_events_adaptor.on("app_mention")
 def app_mentioned(event_data):
     channel = event_data["event"]["channel"]
     text = event_data["event"]["text"]
+
+    # print(channel)
+    # print(text)
 
     keywords = lin_pred(text)
     slack_web_client.chat_postMessage(
--- multi_linear.py
+++ multi_linear.py
@@ -17,15 +17,15 @@ Y는 Sales column 에 해당하는 데이터를 저장해야 합니다.
 """
 
 # Req 1-1-1. advertising.csv 데이터 읽고 저장
-csv_data = np.loadtxt("advertising.csv",skiprows=1, delimiter=",",usecols = range(1,5))
+csv_data = np.loadtxt("advertising.csv", skiprows=1, delimiter=",", usecols=range(1, 5))
 
-X = csv_data[:,0:3]
-Y = csv_data[:,3:4]
-#print(X)
-#print(Y)
+X = csv_data[:, 0:3]
+Y = csv_data[:, 3:4]
+# print(X)
+# print(Y)
 
 # Req 1-1-2. 학습용 데이터와 테스트용 데이터로 분리합니다.
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 """
 Req 1-2-1.
@@ -62,9 +62,10 @@ Variance score값을 출력합니다.
 https://scikit-learn.org/stable/index.html
 """
 # Req. 1-3-2. Mean squared error 계산
-print("Mean squared error: %.2f" % mean_squared_error(Y_test,X_test_pred))
+print("Mean squared error: %.2f" % mean_squared_error(Y_test, X_test_pred))
 # Req. 1-3-3. Variance score 계산
-print("Variance score: %.2f" % r2_score(Y_test,X_test_pred))
+print("Variance score: %.2f" % r2_score(Y_test, X_test_pred))
+
 
 # Req. 1-4-1.
 
@@ -74,28 +75,31 @@ def expected_sales(tv, rd, newspaper, beta_0, beta_1, beta_2, beta_3):
     트레이닝된 모델의 weight 들이 beta_0, beta_1, beta_2, beta_3 일 때
     예상되는 Sales 값을 출력합니다.
     """
-    rs = ((tv * beta_0 + beta_3) + (rd * beta_1 + beta_3) + (newspaper * beta_2 + beta_3))
+    rs = ((tv * beta_0) + (rd * beta_1) + (newspaper * beta_2) + beta_3)
     return rs
+
 
 # Req. 1-4-2.
 # test 데이터에 있는 값을 직접적으로 넣어서 예상 판매량 값을 출력합니다.
 print("TV: {}, Radio: {}, Newspaper: {} 판매량: {}".format(
-   X_test[3][0],X_test[3][1],X_test[3][2],Y_test[3]))
+    X_test[3][0], X_test[3][1], X_test[3][2], Y_test[3]))
 
 print("예상 판매량: {}".format(expected_sales(
-       float(X_test[3][1]),float(X_test[3][1]),float(X_test[3][2]), beta_0, beta_1, beta_2, beta_3)))
+    float(X_test[3][0]), float(X_test[3][1]), float(X_test[3][2]), beta_0, beta_1, beta_2, beta_3)))
 
 """
 Req. 1-5. pickle로 lrmodel 데이터 저장
 파일명: model.clf
 """
-with open("model.clf","wb") as fw:
-    pickle.dump(lrmodel.predict(X_test), fw)
-# with open("model.clf","rb") as fr:
-#     data = pickle.load(fr)
+with open("model.clf", "wb") as fw:
+    pickle.dump(lrmodel, fw)
+# joblib.dump(lrmodel, 'model.clf')
+
+loaded_model = pickle.load(open("model.clf", 'rb'))
 
 # Linear Regression Algorithm Part
 # 아래의 코드는 심화 과정이기에 사용하지 않는다면 주석 처리하고 실행합니다.
+
 
 """
 Req. 3-1-1.
@@ -113,53 +117,71 @@ Linear Regression 학습을 위한 알고리즘입니다.
 4) 가중치 값들 리턴
 
 """
-def N_LinearRegression(X, Y, iters):
+
+
+def N_LinearRegression(X, Y, iters, learning_rate):
     """
-    초기값 beta_0, beta_1, beta_2, beta_3 = 0 
+    초기값 beta_0, beta_1, beta_2, beta_3 = 0
     여러가지 초기값을 실험해봅니다..
     초기값에 따라 iters간의 관계를 확인 가능합니다.
     """
-    beta_x = None
-    beta_3 = None
-    
-    #행렬 계산을 위하여 Y데이터의 사이즈를 (len(Y),1)로 저장합니다.
-    Y=Y.reshape(-1,1)
-    
+    beta_x = [0, 0, 0]
+    beta_3 = [0]
+
+    # 행렬 계산을 위하여 Y데이터의 사이즈를 (len(Y),1)로 저장합니다.
+    Y = Y.reshape(len(Y), 1)
+    print(Y)
     for i in range(iters):
-        #실제 값 y와 예측 값(prediction()함수를 사용)의 차이를 계산하여 error를 정의합니다.
-        error = None
-        #gradient_beta함수를 통하여 델타값들을 업데이트 합니다.
-        beta_x_delta, beta_3_delta = gradient_beta(None)
-        beta_x -= None
-        beta_3 -= None
-        
+        # 실제 값 y와 예측 값(prediction()함수를 사용)의 차이를 계산하여 error를 정의합니다.
+        error = Y - prediction(beta_x, beta_3, X)
+        # print(Y)
+        # print(error)
+        # gradient_beta함수를 통하여 델타값들을 업데이트 합니다.
+        beta_x_delta, beta_3_delta = gradient_beta(X, error, learning_rate)
+        # print(beta_x_delta, beta_3_delta)
+        beta_x -= beta_x_delta
+        beta_3 -= beta_3_delta
+
     return beta_x, beta_3
+
 
 """
 Req. 3-1-2.
 prediction():
 beta값들을 받아서 예측값을 계산합니다.
 X행렬의 크기와 beta의 행렬 크기를 맞추어 계산합니다.
+
 """
-"""
-def prediction(None):
+
+
+def prediction(beta_x, beta_3, X):
     # 예측 값을 계산하는 식을 만든다.
-    equation = None
-    
+    # print(beta_x)
+    # print(X)
+    equation = [0]*len(X)
+    # print(equation)
+    for i in range(len(X)):
+        tmp = np.dot(beta_x, X[i])
+        # print(tmp)
+        equation[i] = tmp + beta_3[0]
+
+        # equation = beta_x[0] * X[i][0] + beta_x[1] * X[i][1] + beta_x[2] * X[i][2] + beta_3[0]
+    # print(equation)
     return equation
-"""
+
+
 """
 Req. 3-1-3.
 gradient_beta():
 beta값에 해당되는 gradient값을 계산하고 learning rate를 곱하여 출력합니다.
 """
-    
-def gradient_beta(X,error,lr):
+
+
+def gradient_beta(X, error, lr):
     # beta_x를 업데이트하는 규칙을 정의한다.
-    beta_x_delta = None
+    beta_x_delta = sum(X * (error**2))*lr / len(X)
     # beta_3를 업데이트하는 규칙을 정의한다.
-    beta_3_delta = None
-    
+    beta_3_delta = sum(error**2)*lr / len(error)
     return beta_x_delta, beta_3_delta
 
 
@@ -167,34 +189,32 @@ def gradient_beta(X,error,lr):
 
 # Req 3-2-4. challenge
 # 학습률(learning rate)를 설정합니다. (권장: 1e-3 ~ 1e-6)
-learning_rate = None
+learning_rate = 1e-6
 # 반복 횟수(iteration)를 설정합니다. (자연수)
-iteration = None
+iteration = 1
 
 # Req. 3-2-1. 모델 학습
-N_beta_x, N_beta_3  = N_LinearRegression(None)
+N_beta_x, N_beta_3 = N_LinearRegression(X_train, Y_train, iteration, learning_rate)
 
 # Req. 3-2-2. 학습된 가중치 저장
 print("\nN_LinearRegression의 결과물")
-print("beta_0: %f" % None)
-print("beta_1: %f" % None)
-print("beta_2: %f" % None)
-print("beta_3: %f" % None)
+print("beta_0: %f" % N_beta_x[0])
+print("beta_1: %f" % N_beta_x[1])
+print("beta_2: %f" % N_beta_x[2])
+print("beta_3: %f" % N_beta_3[0])
 
 # Req. 3-3-1. 테스트 데이터의 예측 label값 계산
 # X_test_pred에 테스트 데이터에 대한 예상 판매량을 모두 구하여 len(y_test) X 1 의 크기를 갖는 열벡터에 저장합니다.
-N_X_test_pred = None
+N_X_test_pred = prediction(N_beta_x,N_beta_3,X_test)
 
 # Req. 3-3-2. Mean squared error 계산
-print("Mean squared error: %.2f" % None)
+print("Mean squared error: %.2f" % mean_squared_error(Y_test, X_test_pred))
 # Req. 3-3-3. Variance score 계산
-print("Variance score: %.2f" % None)
+print("Variance score: %.2f" % r2_score(Y_test, X_test_pred))
 
 # Req. 3-4-1. 예상 판매량 출력
 print("TV: {}, Radio: {}, Newspaper: {} 판매량: {}".format(
-   X_test[3][0],X_test[3][1],X_test[3][2],Y_test[3]))
+    X_test[3][0], X_test[3][1], X_test[3][2], Y_test[3]))
 
 print("예상 판매량: {}".format(expected_sales(
-       float(X_test[3][0]),float(X_test[3][1]),float(X_test[3][2]), beta_0, beta_1, beta_2, beta_3)))
-
-
+    float(X_test[3][0]), float(X_test[3][1]), float(X_test[3][2]), beta_0, beta_1, beta_2, beta_3)))
